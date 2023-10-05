e5ae78e32a8411b4edbbd4af335e5adb3c6e8850
tonytor <teme4842@gmail.com>
2023-10-05 17:06:22 +0300

    car showcase

---
+++ components/index.tsx
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer"
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    Navbar,
+    Footer,
+    CustomButton,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+}
